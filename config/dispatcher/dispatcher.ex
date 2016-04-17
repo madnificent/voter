686052ce9c68161706a009ada63fdc2b717afefe
Erika Pauwels <erika.pauwels@tenforce.com>
2016-04-17 12:48:00 +0100

    Add login and registration service to the backend

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -30,6 +30,14 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://plusOne/"
   end
 
+  match "/accounts/*path" do
+    Proxy.forward conn, path, "http://registration/accounts/"
+  end
+
+  match "/sessions/*path" do
+    Proxy.forward conn, path, "http://login/sessions/"
+  end
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
